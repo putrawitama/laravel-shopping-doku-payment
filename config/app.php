536e6bd5da0d2453a69add72acfcee7d10788e43
fathurrachman <putrawitama@gmail.com>
2017-12-09 11:58:52 +0700

    doku

--- config/app.php
+++ config/app.php
@@ -148,6 +148,8 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
+        crocodicstudio\dokularavel\DokuLaravelServiceProvider::class,
+
         /*
          * Application Service Providers...
          */
