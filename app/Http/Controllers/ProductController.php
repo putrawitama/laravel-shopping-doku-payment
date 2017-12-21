fb6fdd50c7b56388465e00972caec3b9d684b4d1
fathurrachman <putrawitama@gmail.com>
2017-12-22 06:49:53 +0700

    test

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -89,7 +89,7 @@ class ProductController extends Controller
 
         $data = [
             'email' => $user->email,
-            'name' => $user->name,
+            'name' => $user->fullname,
             'total' => number_format($total, 2, ".", ""),
             'WORDS' => sha1($total . $this->mallid . $this->skey . $tmerchant),
             'amount' => number_format($total, 2, ".", ""),
