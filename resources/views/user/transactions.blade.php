80aa446adefa8e382cddb68e11a19a2866b3b8cd
Mukti Wibowo <muktiw7@gmail.com>
2017-12-22 08:46:58 +0700

    transaction added

--- resources/views/user/transactions.blade.php
+++ resources/views/user/transactions.blade.php
@@ -2,9 +2,30 @@
 
 @section('profile-content')
 		<div class="col-md-12">
-			<h1>Transactions</h1>
-			<div class="row">
-
-			</div>
+			<h1 style="margin-bottom:30px;">Transactions</h1>
+			<table class="table table-striped table-hover">
+				<tr>
+					<th>No.</th>
+					<th>Cart</th>
+					<th>Status</th>
+					<th>Created</th>
+				</tr>
+				@if(count($orders) > 0)
+				<?php $i = 1; ?>
+				@foreach($orders as $order)
+				<tr>
+					<td>{{$i}}</td>
+					<td>{{$order->cart}}</td>
+					<td>{{$order->status}}</td>
+					<td>{{date('d F Y, H:i:s', strtotime($order->created_at))}}</td>
+				</tr>
+				<?php $i++; ?>
+				@endforeach
+				@else
+				<tr>
+					<td colspan="4" class="text-center">You haven't ordered a single thing.</td>
+				</tr>
+				@endif
+			</table>
 		</div>
 @endsection
